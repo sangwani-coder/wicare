e52dd38b452c1943ea12ef671e8c5d05304069f5
zyambo <mightypz2@gmail.com>
2023-04-24 18:49:14 +0200

    update page styles

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,4 @@
-// function readMoreReadLess() {
-//     var dots = document.getElementById("dots");
-//     var moreText = document.getElementClass("more");
-//     var btnText = document.getElementByClass("moreBtn");
-  
-//     if (dots.style.display === "none") {
-//       dots.style.display = "inline";
-//       btnText.innerHTML = "Read more";
-//       moreText.style.display = "none";
-//     } else {
-//       dots.style.display = "none";
-//       btnText.innerHTML = "Read less";
-//       moreText.style.display = "inline";
-//     }
-//   }
-
+// function ro toggle between readmore and less text
 function readMoreReadLess(id) {
     var dots = document.getElementsByClassName("dots")[id - 1];
     var moreText = document.getElementsByClassName("more")[id - 1];
@@ -29,4 +14,3 @@ function readMoreReadLess(id) {
       moreText.style.display = "inline";
     }
   }
-  
